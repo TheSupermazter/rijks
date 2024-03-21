065a80be43abe63f02b388758a70e770dd82891b
LinGreefkens <lingreefkens@outlook.com>
2024-03-21 11:51:11 +0100

    + en - ding

--- public/script/linSkittles.js
+++ public/script/linSkittles.js
@@ -1,8 +1,5 @@
 import "https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"
 
-if (document.title === "vraag4") {
-
-    new Sortable(list, {
-        animation: 300, 
-    });
-}
+new Sortable(list, {
+    animation: 300, // ca 300ms is meestal een mooie tijd
+});
