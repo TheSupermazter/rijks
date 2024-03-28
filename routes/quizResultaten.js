da15c0407235b63350e51a3a076455f897769372
Kim Dekker <kim11dekker@gmail.com>
2024-03-28 13:49:40 +0100

    change value to endpoint :(

--- routes/quizResultaten.js
+++ routes/quizResultaten.js
@@ -1,41 +1,27 @@
-module.exports = () => {
+module.exports = ({usersCollection}) => {
 
 const express = require('express');
 const router = express.Router();
 
+const getEndpoints = require('../utils/getEndpoints'); // naar de function die de endpoints verbind aan het antwoord
+
 // QUIZRESULTATEN
 
-router.get('/', (req, res) => {
-    res.render('quizResultaten');
-});
+router.get('/', async (req, res) => {
+    try {
+        if (!req.session.user) { //als de user nog niet is ingelogd
+            return res.redirect('/login');
+        }
 
+        const userId = req.session.user._id; //haal user_id op
+        let endpoints = await getEndpoints(userId.quizAntwoorden); //endpoints komt uit /getEndpoints > plakt het juiste endpoint aan de keuze vd user
 
-
-
-router.post('/quizResultaten', async (req, res) => {
-    const { answer, questionNumber } = req.body;
-    const questionIndex = parseInt(questionNumber) - 1; // Omdat de array zero-based is
-    
-    // Controleer of de vraagnummer en het antwoord geldig zijn
-    if (questionIndex >= 0 && questionIndex < questionMappings.length && questionMappings[questionIndex].hasOwnProperty(answer)) {
-        // Krijg het bijbehorende URL-endpoint voor de keuze
-        const endPoint = questionMappings[questionIndex][answer];
-        
-        // Construeer de volledige URL
-        const baseURL = "https://www.rijksmuseum.nl/api/nl/collection?key=mFMeRfGA";
-        const URL = baseURL + endPoint;
-        
-        // Voer hier je fetch-actie uit met de geconstrueerde URL
-        // await fetch(URL);
-        
-        // Hier kun je verdere verwerking doen met de verkregen gegevens
-        // en een reactie terugsturen naar de gebruiker
-        res.send(`Je hebt gekozen voor ${answer}. URL: ${URL}`);
-    } else {
-        // Ongeldige vraagnummer of antwoord, stuur een foutmelding terug naar de gebruiker
-        res.status(400).send('Ongeldige vraagnummer of antwoord');
+        res.render('quizResultaten', { endpoints }); // Voeg endpoints toe aan de data die naar de view wordt gestuurd
+    } catch (error) {
+        console.error(error);
     }
 });
+
 
 return router;
 
--- routes/vragen.js
+++ routes/vragen.js
@@ -1,18 +1,32 @@
-module.exports = function({ usersCollection, vragenCollection }) {
+module.exports = ( {usersCollection, vragenCollection} ) => {
 
     const { ObjectId } = require('mongodb');
 
     const express = require('express');
     const router = express.Router();
-    
+
     // VRAGEN
     
     router.get('/:number', async (req, res) => {
         const number = parseInt(req.params.number, 10);
         const vraag = await vragenCollection.findOne({ "number": number });
-        res.render('vragen', { vraag: vraag, nextId: number + 1, prevId: number - 1}); //zorgt ervoor dat de http > vragen/ +1 worden of -1 bij navigeren van de knoppen
-    });
+
+    // loop over vraag objecten
+    for (let key in vraag) {
+        if (Array.isArray(vraag[key]) && key === 'choices') {
+            // Log alleen de "endpoint" waarden van de objecten in de "choices" array
+            console.log(`Endpoints:`);
+            vraag[key].forEach(choice => {
+                console.log(choice.endpoint);
+            });
+        }
+    }
     
+        // req.session.vraag = vraag; // Sla de vraag op in de session
+    
+        res.render('vragen', { vraag: vraag, nextId: number + 1, prevId: number - 1});
+    });
+
     
     router.post('/', async (req, res) => {
         const questionNumber = req.body.questionNumber; //nummer van de vraag > nodig om de geantwoordde  vraag aan het juiste vraagnummer te koppelen
@@ -37,7 +51,37 @@ module.exports = function({ usersCollection, vragenCollection }) {
             res.redirect(`/login`);
         }
     });
-    
+
+
+// Wanneer de gebruiker een keuze heeft gemaakt
+// router.post('/', async (req, res) => {
+//     const antwoord = req.body.answer; //een antwoord in de form
+//     const navigate = req.body.navigate; // navigate is de buttonnaam van de next en prev buttons
+
+//     const user = req.session.user;
+//     const vraag = req.session.vraag; // Haal de vraag uit de session
+
+//     if (user && vraag) { //als de user is verbonden en ingelogd en de vraag bestaat
+//         // Zoek het gekozen antwoord in de choices
+//         const gekozenAntwoord = vraag.choices.find(choice => choice.name === antwoord);
+
+//         const result = await usersCollection.updateOne( //update het volgende in de DB
+//             { _id: new ObjectId(user._id) }, //op het user id uit de session cookie
+//             { $set: { [`quizAntwoorden.${vraag.number}`]: gekozenAntwoord.endpoint } } // set > pas aan, of voeg toe > vraagnummer: [gegeven antwoord]
+//         );
+//         if (result) { // als er iets wordt geupdate in de DB
+//             if (navigate) { //redirect naar het volgende vraagnummer
+//                 res.redirect(`/vragen/${navigate}`); //vragen + het vraagnummer die ik uit de form haal
+//             } else {
+//                 res.redirect(`/quizResultaten`); //anders, direct naar de quizResultaten
+//             }
+//         }
+//     } else { // als  hij niet ingelogd is, dan word doorgestuurd naar login
+//         res.redirect(`/login`);
+//     }
+// });
+
+
     return router;
     
     }
